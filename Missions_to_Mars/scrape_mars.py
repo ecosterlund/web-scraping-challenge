9e0ae37b4f761dae05aa6b101abd35837b2f64a2
ecosterlund <erikosterlund@comcast.net>
2021-03-21 21:29:50 -0400

    transferring jupyter notebook to scrape_mars.py

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -4,42 +4,114 @@ import time
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-def scrape_info():
+def scrape():
     # Set up Splinter
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
-    # Visit Nasa Mars News
-    url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-    browser.visit(url)
-
-    time.sleep(1)
-
-    # Scrape page into Soup
+    #--------------------NASA NEWS SCRAPE-----------------------------
+    # URL of page to be scraped
+    news_url = 'https://mars.nasa.gov/news/'
+    browser.visit(news_url)
     html = browser.html
-    soup = bs(html, "html.parser")
+    # Create BeautifulSoup object; parse with 'html.parser'
+    news_soup = bs(html, 'html.parser')
 
-    # Get the article title
-    news_title = soup.find('div', id='content_title')
+    content_title = news_soup.find_all(class_='content_title')
+    body_article = news_soup.find_all(class_='article_teaser_body')
 
-    # Get the news paragraph description
-    news_p = soup.find('div', id='article_teaser_body')
+    news_title=[]
+    news_paragraph = []
 
-    # # Get the max avg temp
-    # max_temp = avg_temps.find_all('strong')[1].text
+    for title in content_title:
+        try:
+            news_title.append(title.a.text.strip())
+        except:
+            pass
+        
+    for body in body_article:
+        try:
+            news_paragraph.append(body.text.strip())
+        except:
+            pass
+        
+    recent_news = news_title[0]
+    recent_paragraph = news_paragraph[0]
+    news_dict = {}
+    news_dict['news title'] = recent_news
+    news_dict['news paragraph'] = recent_paragraph
 
-    # # BONUS: Find the src for the sloth image
-    # relative_image_path = soup.find_all('img')[2]["src"]
-    # sloth_img = url + relative_image_path
+    #-----------------------JPL SCRAPE------------------------
+    # JPL Website
+    jpl_url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
 
-    # Store data in a dictionary
-    nasa_data = {
-        "news_title": news_title,
-        "news_p": news_p,
-    }
+    # Open chrome and visit url
+    browser.visit(jpl_url)
 
-    # Close the browser after scraping
+    html = browser.html
+
+    jpl_soup = bs(html, 'html.parser')
+
+    main_image = jpl_soup.find_all('img', class_='headerimage')
+    for image in main_image:
+        featured_image_url = f"https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{image['src']}"
+    jpl_dict = {}
+    jpl_dict['img url'] = featured_image_url
+    
+
+    #---------------------------MARS FACTS SCRAPE------------------
+    # Scraped table containing facts about the planet
+    mars_url = 'https://space-facts.com/mars/'
+
+    # Read url and returned a list of DataFrames
+    mars_table = pd.read_html(mars_url)
+    mars_table = mars_table[1]
+
+    # Converted data to HTML table string
+    html_mars_table = mars_table.to_html()
+    mars_dict = {}
+    mars_dict['mars table'] = html_mars_table
+
+    #-------------------HEMISPHERES SCRAPE-------------------------
+    hemispheres_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+
+    browser.visit(hemispheres_url)
+
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    names = soup.find_all(class_='description')
+
+    # Create a list of hemisphere names to be used to iterate over them to navigate on the next loop
+    hemisphere_list = []
+    for name in names:
+        hemisphere_list.append(name.a.h3.text)
+
+    # Navigate through all pages and get the full resolution hemisphere image
+    browser.visit(hemispheres_url)
+    hemisphere_image_urls=[]
+    for i in range(len(hemisphere_list)):
+            html = browser.html
+            soup = bs(html, 'html.parser')
+            browser.click_link_by_partial_text(hemisphere_list[i])
+            html = browser.html
+            soup = bs(html, 'html.parser')
+            title = hemisphere_list[i]
+            img_url = soup.find(class_='downloads')
+                
+            hemisphere_dict = {}
+            hemisphere_dict['title'] = title
+            hemisphere_dict['img_url'] = img_url.a['href']
+            hemisphere_image_urls.append(hemisphere_dict)
+            browser.back()
+
+    #Adding everything into dictionaries/lists
+    nasa_data = []
+    nasa_data.append(news_dict)
+    nasa_data.append(jpl_dict)
+    nasa_data.append(mars_dict)
+    nasa_data.append(hemisphere_dict)
+
+    #Close browser with splinter
     browser.quit()
 
-    # Return results
     return nasa_data
